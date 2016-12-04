72e3e95994a836b0bf686715371247b909394337
Juan José Salvador Piedra <juanjosalvador@openmailbox.org>
2016-12-04 22:51:23 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp   = require('gulp');
 var sass   = require('gulp-sass');
 var minify = require('gulp-clean-css');
 var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
 
 gulp.task('sass', function () {
     gulp.src('./app/sass/*.scss')
